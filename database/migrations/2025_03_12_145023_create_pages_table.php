8a63cf9e78a115f43a5dbb0bc1dbef51e58f28b4
Nikolai Filatov <nikolai.filatov@student.ucll.be>
2025-03-13 08:45:54 +0100

    WIP Database

---
+++ database/migrations/2025_03_12_145023_create_pages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->integer('id')->autoIncrement();
+            $table->string('name', 25);
+            $table->string('content',1024);
+            $table->tinyInteger('access_level');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+            $table->string('description', 255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('pages', function (Blueprint $table) {
+            //
+        });
+    }
+};
