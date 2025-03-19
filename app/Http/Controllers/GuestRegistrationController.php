ce5d79c7031f3445ea257bef254b8b7d8a399bf6
Undead <110314402+undead2146@users.noreply.github.com>
2025-03-19 10:55:15 +0100

    Added submitBasicInfo() function to the GuestRegistrationController

--- app/Http/Controllers/GuestRegistrationController.php
+++ app/Http/Controllers/GuestRegistrationController.php
@@ -1,1 +1,49 @@
+<?php
 
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Models\Traveller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\View\View;
+
+class GuestRegistrationController extends Controller
+{
+    const SESSION_KEY = 'guest_registration';
+    
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('user-access:guest');
+    }
+    // TODO: Show Basic Info Form
+    public function showBasicInfoForm(Request $request){}
+    
+    // Submit Basic Info Form
+    public function submitBasicInfo(Request $request)
+    {
+        $validated = $request->validate([
+            'trip' => 'required|string|max:255',
+            'student_number' => 'required|string|max:255',
+            'education' => 'required|string|max:255',
+            'major' => 'required|string|max:255',
+        ]);
+
+        // Get current session data and update it
+        $registration = $request->session()->get(self::SESSION_KEY, []);
+        $registration = array_merge($registration, $validated, ['step' => 2]); // IMPORTANT: save current steps so we can the progress in a partial view
+        
+        // Save updated data back to session
+        $request->session()->put(self::SESSION_KEY, $registration);
+
+        return redirect()->route('guest.registration.personal-info');
+    }
+    // TODO Submit Basic Info Form
+    public function submitBasicInfo(Request $request){}
+
+    // TODO Show Personal Info Form
+    public function showPersonalInfoForm(Request $request){}
+
+}
