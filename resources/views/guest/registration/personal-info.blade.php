27e3d7fc202687393d16d8d48d48d9373e19fb18
lucasvsw <lucas.vanschoenwinkel@student.ucll.be>
2025-03-20 10:01:30 +0100

    Nederlands Persoonlijke info

--- resources/views/guest/registration/personal-info.blade.php
+++ resources/views/guest/registration/personal-info.blade.php
@@ -1,3 +1,5 @@
+{{-- filepath:
+c:\Users\lucas\Downloads\Laragon\www\techreizen\resources\views\guest\registration\personal-info.blade.php --}}
 @extends('layouts.app')
 
 @section('content')
@@ -7,7 +9,7 @@
                 <div class="card">
                     <div class="card-header">
                         <div class="d-flex justify-content-between align-items-center">
-                            <span>{{ __('Registration - Personal Information') }}</span>
+                            <span>{{ __('Registratie - Persoonlijke Informatie') }}</span>
                         </div>
                     </div>
 
@@ -18,12 +20,12 @@
                             @csrf
 
                             <div class="mb-4">
-                                <h5 class="border-bottom pb-2">{{ __('Personal Information') }}</h5>
+                                <h5 class="border-bottom pb-2">{{ __('Persoonlijke Informatie') }}</h5>
                             </div>
 
                             <div class="row mb-3">
                                 <label for="first_name"
-                                    class="col-md-4 col-form-label text-md-end">{{ __('First Name') }}</label>
+                                    class="col-md-4 col-form-label text-md-end">{{ __('Voornaam*') }}</label>
                                 <div class="col-md-6">
                                     <input id="first_name" type="text"
                                         class="form-control @error('first_name') is-invalid @enderror" name="first_name"
@@ -38,7 +40,7 @@
 
                             <div class="row mb-3">
                                 <label for="last_name"
-                                    class="col-md-4 col-form-label text-md-end">{{ __('Last Name') }}</label>
+                                    class="col-md-4 col-form-label text-md-end">{{ __('Achternaam*') }}</label>
                                 <div class="col-md-6">
                                     <input id="last_name" type="text"
                                         class="form-control @error('last_name') is-invalid @enderror" name="last_name"
@@ -52,14 +54,14 @@
                             </div>
 
                             <div class="row mb-3">
-                                <label for="gender" class="col-md-4 col-form-label text-md-end">{{ __('Gender') }}</label>
+                                <label for="gender" class="col-md-4 col-form-label text-md-end">{{ __('Geslacht*') }}</label>
                                 <div class="col-md-6">
                                     <select id="gender" class="form-control @error('gender') is-invalid @enderror"
                                         name="gender" required>
-                                        <option value="">-- Select Gender --</option>
-                                        <option value="male" {{ (old('gender', $registration->gender ?? '') == 'male') ? 'selected' : '' }}>Male</option>
-                                        <option value="female" {{ (old('gender', $registration->gender ?? '') == 'female') ? 'selected' : '' }}>Female</option>
-                                        <option value="other" {{ (old('gender', $registration->gender ?? '') == 'other') ? 'selected' : '' }}>Other</option>
+                                        <option value="">{{ __('-- Selecteer Geslacht --') }}</option>
+                                        <option value="male" {{ (old('gender', $registration->gender ?? '') == 'male') ? 'selected' : '' }}>Man</option>
+                                        <option value="female" {{ (old('gender', $registration->gender ?? '') == 'female') ? 'selected' : '' }}>Vrouw</option>
+                                        <option value="other" {{ (old('gender', $registration->gender ?? '') == 'other') ? 'selected' : '' }}>Anders</option>
                                     </select>
                                     @error('gender')
                                         <span class="invalid-feedback" role="alert">
@@ -71,7 +73,7 @@
 
                             <div class="row mb-3">
                                 <label for="date_of_birth"
-                                    class="col-md-4 col-form-label text-md-end">{{ __('Date of Birth') }}</label>
+                                    class="col-md-4 col-form-label text-md-end">{{ __('Geboortedatum*') }}</label>
                                 <div class="col-md-6">
                                     <input id="date_of_birth" type="date"
                                         class="form-control @error('date_of_birth') is-invalid @enderror"
@@ -87,7 +89,7 @@
 
                             <div class="row mb-3">
                                 <label for="place_of_birth"
-                                    class="col-md-4 col-form-label text-md-end">{{ __('Place of Birth') }}</label>
+                                    class="col-md-4 col-form-label text-md-end">{{ __('Geboorteplaats*') }}</label>
                                 <div class="col-md-6">
                                     <input id="place_of_birth" type="text"
                                         class="form-control @error('place_of_birth') is-invalid @enderror"
@@ -103,7 +105,7 @@
 
                             <div class="row mb-3">
                                 <label for="nationality"
-                                    class="col-md-4 col-form-label text-md-end">{{ __('Nationality') }}</label>
+                                    class="col-md-4 col-form-label text-md-end">{{ __('Nationaliteit') }}</label>
                                 <div class="col-md-6">
                                     <input id="nationality" type="text"
                                         class="form-control @error('nationality') is-invalid @enderror" name="nationality"
@@ -117,11 +119,11 @@
                             </div>
 
                             <div class="mb-4 mt-4">
-                                <h5 class="border-bottom pb-2">{{ __('Address Information') }}</h5>
+                                <h5 class="border-bottom pb-2">{{ __('Adresinformatie') }}</h5>
                             </div>
 
                             <div class="row mb-3">
-                                <label for="address" class="col-md-4 col-form-label text-md-end">{{ __('Address') }}</label>
+                                <label for="address" class="col-md-4 col-form-label text-md-end">{{ __('Adres*') }}</label>
                                 <div class="col-md-6">
                                     <input id="address" type="text"
                                         class="form-control @error('address') is-invalid @enderror" name="address"
@@ -135,7 +137,7 @@
                             </div>
 
                             <div class="row mb-3">
-                                <label for="city" class="col-md-4 col-form-label text-md-end">{{ __('City') }}</label>
+                                <label for="city" class="col-md-4 col-form-label text-md-end">{{ __('Stad*') }}</label>
                                 <div class="col-md-6">
                                     <input id="city" type="text" class="form-control @error('city') is-invalid @enderror"
                                         name="city" value="{{ old('city', $registration->city ?? '') }}" required>
@@ -148,7 +150,7 @@
                             </div>
 
                             <div class="row mb-3">
-                                <label for="country" class="col-md-4 col-form-label text-md-end">{{ __('Country') }}</label>
+                                <label for="country" class="col-md-4 col-form-label text-md-end">{{ __('Land*') }}</label>
                                 <div class="col-md-6">
                                     <input id="country" type="text"
                                         class="form-control @error('country') is-invalid @enderror" name="country"
@@ -164,10 +166,10 @@
                             <div class="row mb-0">
                                 <div class="col-md-6 offset-md-4">
                                     <a href="{{ route('guest.registration.basic-info') }}" class="btn btn-secondary me-2">
-                                        {{ __('Previous') }}
+                                        {{ __('Vorige') }}
                                     </a>
                                     <button type="submit" class="btn btn-primary">
-                                        {{ __('Next') }}
+                                        {{ __('Volgende') }}
                                     </button>
                                 </div>
                             </div>
