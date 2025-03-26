cd740d4e76bdb7c84b98196b328accb0889a125b
inaskie <inas.mezouri@student.ucll.be>
2025-03-26 20:03:26 +0100

    Carrousel alleen in de home tonen fix + volledige html toonbaar op de home

--- resources/views/components/layout/home.blade.php
+++ resources/views/components/layout/home.blade.php
@@ -5,24 +5,41 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
+  
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Fonts -->
+
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
-    <!-- Scripts -->
+   
     @vite(['resources/sass/app.scss', 'resources/js/app.js','resources/css/app.css'])
+    
+    <style>
+        body {
+            padding-top: 160px; 
+        }
+        .carousel-container {
+            width: 100%;
+            max-width: 1200px;
+            margin: 0 auto;
+        }
+        .carousel-item img {
+            width: 100%;
+            height: auto;
+            max-height: 200px;
+            object-fit: contain;
+        }
+    </style>
 </head>
 
 <body>
-   
-    <div class= "fixed-top" style="z-index: 100">
+    <!-- ENKEL DEZE fixed-top DIV HOUDEN -->
+    <div class="fixed-top" style="z-index: 100; background: white;">
         <div class="d-flex flex-row bg-light">
-            <div class="p-2"> <img src="{{ asset('images/ucll_logo.png') }}" class="rounded" alt="logo ucll"></div>
+            <div class="p-2"><img src="{{ asset('images/ucll_logo.png') }}" class="rounded" alt="logo ucll"></div>
             <div class="d-flex flex-column">
                 <div class="pt-3">
                     <h1 class="text-danger">TECHNOLOGIE</h1>
@@ -35,30 +52,26 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/home') }}" 
-                style="padding: 5px; color: black; white-space: nowrap; display: inline-block;">
-                 Home
-             </a>
-             <a class="navbar-brand" href="{{ url('/voorbeeldreizen') }}" 
-             style="padding: 5px; color: black; white-space: nowrap; display: inline-block;">
-              Voorbeeldreis
-          </a>
-          <a class="navbar-brand" href="{{ url('/home') }}" 
-          style="padding: 5px; color: black; white-space: nowrap; display: inline-block;">
-           Contact
-       </a>
+                   style="padding: 5px; color: black; white-space: nowrap; display: inline-block;">
+                   Home
+                </a>
+                <a class="navbar-brand" href="{{ url('/voorbeeldreizen') }}" 
+                   style="padding: 5px; color: black; white-space: nowrap; display: inline-block;">
+                   Voorbeeldreis
+                </a>
+                <a class="navbar-brand" href="{{ url('/home') }}" 
+                   style="padding: 5px; color: black; white-space: nowrap; display: inline-block;">
+                   Contact
+                </a>
                 
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    
-                    
-                    <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
@@ -68,17 +81,15 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                   data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
-
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                       onclick="event.preventDefault();
+                                                document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
-
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
                                     </form>
@@ -89,26 +100,30 @@
                 </div>
             </div>
         </nav>
-        {{-- <div class = "row justify-content-center">
-            <div class="text-center">
-                <div id="images" class="carousel slide carousel-fade" data-bs-ride="carousel">
-                    <!-- slideshow images -->
-                    <div class="carousel-inner mx-auto">
-                        <div class="carousel-item active">
-                            <img src="{{ asset('images/header1.jpg') }}" class="img-fluid " alt="header 1">
-                        </div>
-                        <div class="carousel-item">
-                            <img src="{{ asset('images/header2.jpg') }}" class="img-fluid " alt="header 2">
-                        </div>
-                        <div class="carousel-item">
-                            <img src="{{ asset('images/header3.jpg') }}" class="img-fluid " alt="header 2">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div> --}}
-        <main class="py-4">
-            {{ $slot }}
-        </main>
     </div>
 
+<!-- Carrousel -->
+@if(request()->routeIs('home')) <!-- Carrousel alleen op de homepagina -->
+    <div class="carousel-container mt-3">
+        <div id="images" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="{{ asset('images/header1.jpg') }}" alt="header 1" class="img-fluid">
+                </div>
+                <div class="carousel-item">
+                    <img src="{{ asset('images/header2.jpg') }}" alt="header 2" class="img-fluid">
+                </div>
+                <div class="carousel-item">
+                    <img src="{{ asset('images/header3.jpg') }}" alt="header 3" class="img-fluid">
+                </div>
+            </div>
+        </div>
+    </div>
+@endif
+
+    <!-- Main content -->
+    <main class="container py-4" style="margin-top: 20px;">
+        {{ $slot }}
+    </main>
+</body>
+</html>
