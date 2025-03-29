6ce4d524ee31cd792c6f4548611eb414757839d0
Nikolai Filatov <nikolai.filatov@student.ucll.be>
2025-03-29 14:47:54 +0100

    Added Text-editor and route

---
+++ resources/views/components/layout/head/texteditor-config.blade.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.min.js"></script>
+<style>
+    .trix-content h1 { font-size: 1.5rem; font-weight: bold; }
+    .trix-content ul { list-style-type: disc; padding-left: 1.5rem; }
+    .trix-content ol { list-style-type: decimal; padding-left: 1.5rem; }
+    .trix-button-group.trix-button-group--file-tools { display: none !important; }
+</style>
--- resources/views/content/editor.blade.php
+++ resources/views/content/editor.blade.php
@@ -1,3 +1,17 @@
-<div>
-    
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Trix Editor in Laravel</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.min.css">
+</head>
+<body>
+    <form method="post">
+        <input id="x" type="hidden" name="content">
+        <trix-editor input="x">Hello, World!</trix-editor>
+    </form>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.min.js"></script>
+</body>
+</html>
