3bd36f9b1ef680eefc17e967c5d00fa9486873e8
RensThielens <rens.thielens@student.ucll.be>
2025-04-03 09:01:47 +0200

    dropdown en button

--- resources/views/content/editor.blade.php
+++ resources/views/content/editor.blade.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Editor</title>
 </head>
+
 <body>
     <textarea name="content" id="content" cols="30" rows="10" class="ckeditor form-control" placeholder="Enter post content here...">
         <div class="document-container">
@@ -18,4 +20,5 @@
     
     <script src="ckeditor/ckeditor.js"></script>
 </body>
+
 </html>
