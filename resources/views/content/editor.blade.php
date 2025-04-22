865de1e988ce1e48d246df79ef05d929d04fee22
Nikolai Filatov <nikolai.filatov@student.ucll.be>
2025-04-22 15:35:47 +0200

    Added image option

--- resources/views/content/editor.blade.php
+++ resources/views/content/editor.blade.php
@@ -58,6 +58,9 @@
         const htmlEditor = document.getElementById('html-editor');
         const pdfChooser = document.getElementById('pdf-chooser');
 
+        htmlEditor.style.display = "block";
+        pdfChooser.style.display = "none";
+
         select.addEventListener('change', function() {
             updateEditorView();
         });
