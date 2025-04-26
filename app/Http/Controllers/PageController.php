9be71e7d756ed0ac296fb99977535c3fa128bdb2
andres-vr <andres.vanreepingen@student.ucll.be>
2025-04-26 14:29:33 +0200

    editor fixes

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -32,7 +32,11 @@ class PageController extends Controller
         } elseif ($routeName == "editor") {
             $pageData = $page->find(1); // Fetch the entire page data
             return view('content.editor', ['page' => $pageData]);
+        } elseif ($routeName == "editor-all") {
+            $pageData = $page->find(1); // Fetch the entire page data
+            return view('content.editor-all', ['page' => $pageData]);
         }
+
     }
 
     /**
@@ -111,19 +115,19 @@ class PageController extends Controller
     }
 
     public function saveEditorContent(Request $request)
-{
-    $request->validate([
-        'content' => 'required',
-        'page_id' => 'required|exists:pages,id'
-    ]);
+    {
+        $request->validate([
+            'content' => 'required',
+            'page_id' => 'required|exists:pages,id'
+        ]);
 
-    $page = PageModel::find($request->page_id);
-    $page->update([
-        'type' => 'html',
-        'content' => $request->input('content')
-    ]);
+        $page = PageModel::find($request->page_id);
+        $page->update([
+            'type' => 'html',
+            'content' => $request->input('content')
+        ]);
 
-    return response()->json(['message' => 'Content saved successfully!']);
-}
+        return response()->json(['message' => 'Content saved successfully!']);
+    }
 
 }
