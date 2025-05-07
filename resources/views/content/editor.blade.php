87ca5a05f1b88fefd7d152c365cd003fd10e6db3
Onezgamez <zeno.raymaekers@student.ucll.be>
2025-05-07 19:46:08 +0200

    navbar toegevoegd aan editor

--- resources/views/content/editor.blade.php
+++ resources/views/content/editor.blade.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Editor</title>
-</head>
-
-<body>
+<x-layout.home>
     @if ($previousRoute == 'home')
     <div id="html-editor">
         <textarea name="content" id="editor" cols="30" rows="10" class="ckeditor form-control">
@@ -134,6 +124,4 @@
         .catch(error => alert("Error: " + error.message));
 });
     </script>
-</body>
-
-</html>
+</x-layout.home>
